a4d1617ed929c6c01f5d55225d8fada45d155cd7
Semih-Sinani <semihsinaniofficial@gmail.com>
2024-10-05 10:46:41 +0200

    Adding Task:12 Reack0

--- resources/views/iframe.blade.php
+++ resources/views/iframe.blade.php
@@ -1,1 +1,23 @@
-<iframe src="https://www.solarsystemscope.com/iframe" width="100%" height="100%" style="min-width:500px; min-height: 400px; border: 2px solid #0f5c6e;"></iframe>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    <style>
+        body{
+            padding: 0px;
+            margin: 0px;
+        }
+        iframe{
+            width: 100%;
+            height: 100vh;
+        }
+    </style>
+</head>
+<body>
+    <iframe src="https://www.solarsystemscope.com/iframe" class=""></iframe>
+
+</body>
+</html>
